d1b93d71aa727fb724cf8281ecf0dd3f817a49de
ReganRK <reganreinal@gmail.com>
2023-07-28 18:50:52 +0700

    Init table
    bikin table tampilin data

--- index.php
+++ index.php
@@ -7,7 +7,27 @@
 </head>
 <body>
     <?php
-        echo "<h1>Hi I'm Regan</h1>"
+        echo "<h1>Hi I'm Regan</h1><br>";
+
+        echo "<center><table>";
+        // Open a file
+        $file = fopen("rumah.csv", "r");
+  
+        // Fetching data from csv file row by row
+        while (($data = fgetcsv($file)) !== false) {
+            // HTML tag for placing in row format
+            echo "<tr>";
+            foreach ($data as $i) {
+                echo "<td>" . htmlspecialchars($i) 
+                    . "</td>";
+            }
+            echo "</tr> \n";
+        }
+  
+        // Closing the file
+        fclose($file);
+
+        echo "</table></center>";
     ?>
 </body>
 </html>
